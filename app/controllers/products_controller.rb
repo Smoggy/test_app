849b3bb6d581d44f6a990e277eb55032c19da987
Smoggy <avatar145@ukr.net>
2014-04-15 19:30:03 +0300

    update delete product

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,7 +1,7 @@
 class ProductsController < ApplicationController
 
 	def index
-
+		@products = Product.all.paginate(page: params[:page])
 	end
 
 	def show
@@ -14,20 +14,29 @@ class ProductsController < ApplicationController
 
 	def create
 		@product = Product.new(product_params)
-    	if @product.save
-      
-      	redirect_to @product
+    	if @product.save 
+      		redirect_to products_path
     	else
       		render 'new'
     	end
 	end
 
-	def update
+	def edit
+		@product = Product.find(params[:id])
+	end
 
+	def update
+		if Product.find(params[:id]).update_attributes(product_params)
+      		redirect_to products_path
+    	else
+      		render 'edit'
+    	end
 	end
 
 	def destroy
-
+		product = Product.find(params[:id])
+		product.destroy
+		redirect_to products_path
 	end
 
 	private 
