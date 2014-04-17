486d3182e142e7c68a95ece7cbb0aa3f402bc2b6
Smoggy <avatar145@ukr.net>
2014-04-17 13:37:55 +0300

    Delete 20140415155248_change_latlong_to_lat.rb

--- db/migrate/20140415155248_change_latlong_to_lat.rb
+++
@@ -1,7 +0,0 @@
-class ChangeLatlongToLat < ActiveRecord::Migration
-  def change
-  	rename_column :products, :lat_long, :lat
-  	change_column :products, :lat, :integer
-  	add_column :products, :long, :integer
-  end
-end
